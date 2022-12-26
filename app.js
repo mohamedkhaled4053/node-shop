6a4bb2e6e14bf82f4440aa5f7050f228fd672479
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-12-26 08:23:06 +0200

    serve html files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
@@ -13,7 +14,7 @@ app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
 app.use((req,res,next)=>{
-  res.status(404).send('<h1>not found</h1>')
+  res.status(404).sendFile(path.join(__dirname,'views','not-found.html'))
 })
 
 app.listen(3000);
