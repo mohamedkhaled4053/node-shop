941126858e4a1e30648104f76eac87d2942f0160
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-12-26 09:51:29 +0200

    separate css files

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const shopRoutes = require('./routes/shop')
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin',adminRoutes)
@@ -14,7 +16,7 @@ app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
 app.use((req,res,next)=>{
-  res.status(404).sendFile(path.join(__dirname,'views','not-found.html'))
+  res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 app.listen(3000);
