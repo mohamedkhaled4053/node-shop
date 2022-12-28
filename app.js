83c773a1be17ea167dc5ca5ead794675c0a60339
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-12-28 16:18:55 +0200

    404 controller

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { get404 } = require('./controllers/404');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,8 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(get404);
 
 app.listen(3000);
