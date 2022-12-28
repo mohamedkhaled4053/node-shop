3f8b4bacc7b5b484b9e8f13b838d65f7fe047332
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-12-28 13:21:31 +0200

    update for next module

--- routes/shop.js
+++ routes/shop.js
@@ -3,13 +3,20 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-const {products} = require('./admin');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop',{products, title: 'shop', hasProducts: products.length > 0})
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
