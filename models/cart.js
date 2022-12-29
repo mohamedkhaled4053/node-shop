0e36531c2b612c7aa3cbc33d73970e437134d663
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-12-29 18:59:02 +0200

    handle cart products amount

--- models/cart.js
+++ models/cart.js
@@ -7,8 +7,18 @@ const cartPath = path.join(require.main.filename, '..', 'data', 'cart.json');
 module.exports = class Cart {
   static addToCart(product) {
     getDataFromFile(cartPath, (cart) => {
-      cart.push(product);
-
+      // see if we have the product in cart
+      let existed = cart.find((p) => p.id === product.id);
+      // increase amount if we have it
+      if (existed) {
+        let index = cart.indexOf(existed)
+        existed.amount ++
+        cart[index] = existed
+      } else {
+        // if we don't have it add it
+        cart.push({...product, amount: 1})
+      }
+      
       fs.writeFile(cartPath, JSON.stringify(cart), (err) => {
         console.log(err);
       });
