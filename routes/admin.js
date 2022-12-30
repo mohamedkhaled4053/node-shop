a408d3843b51e4445cf15caa7a69eb1aa7731975
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-12-30 13:40:49 +0200

    prepare edit functionality

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
-const {getAddProduct,getProducts,postAddProduct} = require('../controllers/admin');
+const {
+  getAddProduct,
+  getProducts,
+  postAddProduct,
+} = require('../controllers/admin');
+const Product = require('../models/product');
 
 const router = express.Router();
 
@@ -14,5 +19,17 @@ router.get('/products', getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', postAddProduct);
+
+// /admin/edit-product/:id?edit=true => GET
+router.get('/edit-product/:id', (req, res, next) => {
+  Product.fetchProduct(req.params.id, (product) => {
+    res.render('admin/add-product', {
+      pageTitle: 'edit Product',
+      path: '/admin/edit-product',
+      edit: true,
+      product
+    });
+  });
+});
 
 module.exports = router;
