5809f0a1697469b1772d46b75e2584f6ec566604
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2023-01-02 11:14:10 +0200

    get started with relations

--- app.js
+++ app.js
@@ -13,17 +13,35 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/User');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res,next)=>{
+  User.findByPk(1).then((user)=>{
+    req.user = user
+    next()
+  })
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User,{constraints: true,onDelete:'CASCADE'})
+User.hasMany(Product)
+
 sequelize
-  .sync()
+  .sync().then(()=>{
+    return User.findByPk(1)
+  }).then((user)=>{
+    if (!user) {
+      User.create({name: 'ali',email: 'test@test.com'})
+    }
+  })
   .then(() => {
     app.listen(3000);
   })
---
+++ models/user.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+});
+
+
+module.exports = User
