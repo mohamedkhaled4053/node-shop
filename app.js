654d77bc427f9ea16a951822d9d8f06acc4a6480
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2023-01-18 13:24:34 +0200

    start with sessions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session')
 
 const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
@@ -27,6 +28,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false}))
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,8 @@
 exports.getLogin = (req, res, next) => {
-  isLoggedIn = req.get('Cookie').split('=')[1] === 'true'
+  let isLoggedIn = req.session.isLoggedIn
   res.render('auth/login', { pageTitle: 'login', path: '/login', isLoggedIn });
 };
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true')
+  req.session.isLoggedIn = true
   res.redirect('/')
 };
