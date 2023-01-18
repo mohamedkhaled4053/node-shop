4b56d1d4933f338bf573edd3344ae9c41a181d01
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2023-01-18 13:07:39 +0200

    start with cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', { pageTitle: 'login', path: '/login', isLoggedIn : req.isLoggedIn });
+  isLoggedIn = req.get('Cookie').split('=')[1] === 'true'
+  res.render('auth/login', { pageTitle: 'login', path: '/login', isLoggedIn });
 };
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true
+  res.setHeader('Set-Cookie', 'loggedIn=true')
   res.redirect('/')
 };
